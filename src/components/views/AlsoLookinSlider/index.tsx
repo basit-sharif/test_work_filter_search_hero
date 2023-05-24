31d84aa477f043f0176cd56656329c9a2c30e65f
abdulbasit <ba3568008@gmail.com>
2023-05-25 03:23:57 +0500

    removed 2number 3rd class react-slick library

--- src/components/views/AlsoLookinSlider/index.tsx
+++ src/components/views/AlsoLookinSlider/index.tsx
@@ -1,16 +1,13 @@
 import { ProductType } from "@/components/utils/Types"
 import { FC } from "react"
-import CardSlider from "../Search/subComponents/CardSlider"
-import MultipleItems from "../Slick"
+import CardSlider from "./CardSliderForAlso"
 
 const AlsoLookingSlider: FC<{ villaHouseDataArray: ProductType }> = ({ villaHouseDataArray }) => {
     return (
         <div className='md:pl-14 2xl:pl-0 bg-white'>
             <div className='max-w-7xl mx-auto py-10 space-y-4'>
                 <h3 className="mt-2 text-PrimaryBlack opacity-90">Members looking to travel to your location - Ljubjana</h3>
-                <MultipleItems villaHouseDataArray={villaHouseDataArray.products} />
-                {/* custom slider */}
-                {/* <CardSlider villaHouseDataArray={villaHouseDataArray.products} /> */}
+                <CardSlider villaHouseDataArray={villaHouseDataArray.products} />
             </div>
         </div>
     )
