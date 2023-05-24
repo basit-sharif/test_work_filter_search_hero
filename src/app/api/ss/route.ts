cb26ff968d5140757b87a945c2eb09df4d68df57
abdulbasit <ba3568008@gmail.com>
2023-05-24 11:36:06 +0500

    aaaa

--- src/app/api/ss/route.ts
+++ src/app/api/ss/route.ts
@@ -2,12 +2,8 @@ import * as fs from "fs"
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const url = req.nextUrl;
-    if (url.searchParams.has("filename")) {
-
         try {
-            // ${url.searchParams.get("filename")}
-            let products = await fs.promises.readFile(`./public/ProductData/products.json`, "utf-8");
+            let products = await fs.promises.readFile(`./src/app/ProductData/products.json`, "utf-8");
             products = JSON.parse(products);
             return NextResponse.json({ products })
 
@@ -15,8 +11,4 @@ export async function GET(req: NextRequest) {
             console.log((error as { message: string }).message)
             return NextResponse.json({ message: (error as { message: string }).message });
         }
-
-    } else {
-        return NextResponse.json({ message: "Please put you filename in the url" })
-    }
 }
