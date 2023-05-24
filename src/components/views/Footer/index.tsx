0392435e36200092decdf1a07d81bb8fe00296ef
abdulbasit <ba3568008@gmail.com>
2023-05-25 04:34:13 +0500

    fixed all issues done perfect some filter remaining and all responsive

--- src/components/views/Footer/index.tsx
+++ src/components/views/Footer/index.tsx
@@ -1,9 +1,39 @@
 const Footer = () => {
     return (
         <div className="md:pl-14 2xl:pl-0">
-            <div className="max-w-7xl mx-auto">
-                <h5 className="mb-2 text-xl text-PrimaryBlack text-center lg:text-start">Explore options</h5>
-                <form className="mx-auto lg:mx-0 flex flex-col lg:flex-row w-[85%] justify-self-start p-5 bg-secondaryWhite rounded-lg text-primaryLight items-start lg:items-end justify-start gap-2">
+            <div className="flex flex-col justify-start items-start max-w-7xl mx-auto ">
+                <h5 className="mb-2 text-xl text-PrimaryBlack">Explore options</h5>
+                <form className="flex flex-col md:flex-row items-start md:items-center p-5 bg-secondaryWhite rounded-lg text-primaryLight gap-4">
+                    <div className="flex flex-col">
+                        <label htmlFor="destination">where do you want to go</label>
+                        <input className="mt-2 ring-1 ring-primaryLight focus:outline-none focus:shadow-md text-primaryDark rounded-lg py-2 px-8 flex-grow" id="destination" type="text" placeholder="Search destination" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="data">where do you want to go a</label>
+                        <input className="mt-2 ring-1 ring-primaryLight focus:outline-none focus:shadow-md text-primaryDark rounded-lg py-2 px-16 flex-grow" id="data" type="date" />
+                    </div>
+                    <button
+                        aria-label="Submit button for collection form data"
+                        className="self-end px-16 py-2 bg-primaryLight text-white rounded-md"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div >
+        </div>
+    )
+}
+
+export default Footer
+
+
+
+
+// raw data
+
+{/* <div className="flex flex-col justify-start max-w-7xl mx-auto">
+                <h5 className="mb-2 text-xl text-PrimaryBlack">Explore options</h5>
+                <form className="flex p-5 bg-secondaryWhite rounded-lg text-primaryLight border border-yellow-300">
                     <div className="flex flex-col">
                         <label htmlFor="destination">where do you want to go</label>
                         <input className="mt-2 flex-1 searchInputField" id="destination" type="text" placeholder="Search destination" />
@@ -12,18 +42,11 @@ const Footer = () => {
                         <label htmlFor="data">where do you want to go a</label>
                         <input className="mt-2 flex flex-1 flex-grow searchInputField" id="data" type="date" placeholder="Set data" />
                     </div>
-                    <div>
-                        <button
-                            aria-label="Submit button for collection form data"
-                            className="self-end px-16 py-2 bg-primaryLight text-white rounded-md"
-                        >
-                            Submit
-                        </button>
-                    </div>
+                    <button
+                        aria-label="Submit button for collection form data"
+                        className="self-end px-16 py-2 bg-primaryLight text-white rounded-md"
+                    >
+                        Submit
+                    </button>
                 </form>
-            </div>
-        </div >
-    )
-}
-
-export default Footer
+            </div> */}
